75404192204e6c6ba93446b0bac0f006a55f23e0
MaxLuzhetsky <lujetsky.max@gmail.com>
2022-10-26 16:13:47 +0300

    some changes in task 4

--- homework2/task4.js
+++ homework2/task4.js
@@ -1,23 +1,26 @@
-function checkNumberValues(num){
+function checkNumberValues(num) {
     let numStr = num.toString().split('').map(Number)
-    let sum = numStr.reduce((a,b) => a + b )
+    let sum = numStr.reduce((a, b) => a + b)
     let checkHalves = 0
     let checkDig = 0
-    let arr = numStr.filter((item,index) => numStr.indexOf(item) != index)
- 
-   if((numStr[0] + numStr[1]) === (numStr[2] + numStr[3])){
-       checkHalves = true
-   }else{
-       checkHalves = false
-   }
-   if(arr.length > 0){
-       checkDig = true
-   }else{
-       checkDig = false
-   }
-     
-   console.log(" areHalvesEqual:" + checkHalves)
-   console.log("hasSameDigit:" + checkDig)
-   console.log("sum:" + sum)
- }
- checkNumberValues(5154)
+    let arr = numStr.filter((item, index) => numStr.indexOf(item) != index)
+
+    if ((numStr[0] + numStr[1]) === (numStr[2] + numStr[3])) {
+        checkHalves = true
+    } else {
+        checkHalves = false
+    }
+    if (arr.length > 0) {
+        checkDig = true
+    } else {
+        checkDig = false
+    }
+
+    let obj = {
+        "sum": sum,
+        "hasSameDigit": checkDig,
+        "areHalvesEqual": checkHalves
+    }
+    console.log(obj)
+}
+checkNumberValues(5154)
