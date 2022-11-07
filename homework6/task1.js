f3930b90ca1b34dc23f2fa68f90885945fcb6527
MaxLuzhetsky <lujetsky.max@gmail.com>
2022-11-07 16:51:16 +0200

    task complited

---
+++ homework6/task1.js
@@ -0,0 +1,41 @@
+function quadraticEquation(a, b, c) {
+    let x1 = 0
+    let x2 = 0
+    let error = ""
+    try {
+        if (a === 0 || b === 0 || c === 0) {
+            error = "its not a quadratic equation"
+            throw new SyntaxError(error)
+
+        } else if (!a || !b || !c) {
+            error = "Missing something"
+            throw new SyntaxError(error)
+        }
+
+        let discrim = b * b - 4 * a * c
+        
+
+        if (discrim > 0) {
+            x1 = (-b + Math.sqrt(discrim)) / (2 * a)
+            x2 = (-b - Math.sqrt(discrim)) / (2 * a)
+            console.log(`x1 = ${x1} and x2= ${x2}`)
+
+        } else if (discrim === 0) {
+            x1 = x2 = -b / (2 * a)
+            console.log(`x1 = ${x1} and x2= ${x2}`)
+        } else {
+            let realPart = (-b / (2 * a)).toFixed(2);
+            let imagPart = (Math.sqrt(-discrim) / (2 * a)).toFixed(2);
+
+            console.log(`The roots of quadratic equation are ${realPart} + ${imagPart}i and ${realPart} - ${imagPart}i`
+            )
+        }
+    } catch (err) {
+        console.log(error)
+    }
+
+}
+
+
+
+quadraticEquation(1, 5, 4)
