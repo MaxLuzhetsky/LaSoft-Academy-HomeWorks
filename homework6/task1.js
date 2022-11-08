8944d617730148ffda3674ce982986dfc129225e
MaxLuzhetsky <lujetsky.max@gmail.com>
2022-11-08 03:08:07 +0200

    fixed

--- homework6/task1.js
+++ homework6/task1.js
@@ -12,30 +12,32 @@ function quadraticEquation(a, b, c) {
             throw new SyntaxError(error)
         }
 
-        let discrim = b * b - 4 * a * c
-        
 
-        if (discrim > 0) {
-            x1 = (-b + Math.sqrt(discrim)) / (2 * a)
-            x2 = (-b - Math.sqrt(discrim)) / (2 * a)
-            console.log(`x1 = ${x1} and x2= ${x2}`)
-
-        } else if (discrim === 0) {
-            x1 = x2 = -b / (2 * a)
-            console.log(`x1 = ${x1} and x2= ${x2}`)
-        } else {
-            let realPart = (-b / (2 * a)).toFixed(2);
-            let imagPart = (Math.sqrt(-discrim) / (2 * a)).toFixed(2);
-
-            console.log(`The roots of quadratic equation are ${realPart} + ${imagPart}i and ${realPart} - ${imagPart}i`
-            )
-        }
     } catch (err) {
-        console.log(error)
+        throw err
+    }
+
+    let discrim = b * b - 4 * a * c
+
+
+    if (discrim > 0) {
+        x1 = (-b + Math.sqrt(discrim)) / (2 * a)
+        x2 = (-b - Math.sqrt(discrim)) / (2 * a)
+        return `x1 = ${x1} and x2= ${x2}`
+
+    } else if (discrim === 0) {
+        x1 = x2 = -b / (2 * a)
+        return `x1 = ${x1} and x2= ${x2}`
+    } else {
+        let realPart = (-b / (2 * a)).toFixed(2);
+        let imagPart = (Math.sqrt(-discrim) / (2 * a)).toFixed(2);
+
+        return `The roots of quadratic equation are ${realPart} + ${imagPart}i and ${realPart} - ${imagPart}i`
+
     }
 
 }
 
 
 
-quadraticEquation(1, 5, 4)
+console.log(quadraticEquation(1, 5, 4));
