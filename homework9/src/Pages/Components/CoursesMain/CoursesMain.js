727d8d0a1beaab42d8a03f07e36eab2783078951
MaxLuzhetsky <lujetsky.max@gmail.com>
2022-11-24 13:47:23 +0200

    created course list

--- homework9/src/Pages/Components/CoursesMain/CoursesMain.js
+++ homework9/src/Pages/Components/CoursesMain/CoursesMain.js
@@ -11,37 +11,51 @@ export default function CoursesMain() {
                 return response.json();
             })
             .then((data) => {
-                
+
                 setCourses(data);
-                
+
 
 
             });
 
-           console.log(courses)
+        console.log(courses)
     }, [])
 
 
 
     return (
-        <div className='courses-container' >
+        <div className='courses-container'>
             <h3 className='courses-title' >Pick your perfect course</h3>
-            {courses.map((course) => (
+            <div className='courses-list' >
 
-                <div className='course-content'>
-                    <div key={course.id}>
-                        <img src={course.thumbnail_img_url} ></img>
-                        <div className='name-and-price' >
-                            <h4>{course.name}</h4>
-                            <p>${course.price}</p>
-                        </div>
-                        <div className='course-level'>
-                            <span>Level:</span>
-                            <p>{course.level}</p>
+                {courses.map((course) => (
+
+                    <div className='course-content'>
+                        <div key={course.id}>
+                            <img src={course.thumbnail_img_url} ></img>
+                            <div  className='course-info'>
+                                <div className='name-and-price' >
+                                    <h4>{course.name}</h4>
+                                    <p>${course.price}</p>
+                                </div>
+                                <div className='course-level'>
+                                    <span>Level:</span>
+                                    <p>{course.level}</p>
+                                </div>
+                                <div className='course-duration'>
+                                    <span>Duration</span>
+                                    <div>
+                                    <p>{course.duration}</p>
+                                    <p>{course.periodicity}</p>
+                                    </div>
+                                </div>
+                                <p>{course.description_short}</p>
+                                <button className='course-apply-button'>Apply</button>
+                            </div>
                         </div>
                     </div>
-                </div>
-            ))}
+                ))}
+            </div>
         </div>
     )
 }
